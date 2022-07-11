9042cd8155734714af7d31bcbd4dfa02cc63b6e0
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-07-11 10:35:29 +0000

    First commit.

---
+++ src/main/java/com/mc/gw/McApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mc.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McApiGatewayApplication.class, args);
+	}
+
+}
