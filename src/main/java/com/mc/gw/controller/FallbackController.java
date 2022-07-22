c9a49eb040a17bb1ed70d6501a1ca86c89de68fe
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-07-22 10:02:09 +0000

    Add log to fallback controller.

--- src/main/java/com/mc/gw/controller/FallbackController.java
+++ src/main/java/com/mc/gw/controller/FallbackController.java
@@ -1,5 +1,7 @@
 package com.mc.gw.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,19 +10,24 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(FallbackController.class);
 
     @GetMapping("/customer-address")
     Flux<Void> getCustomerAddressFallback() {
+        LOGGER.error("Customer Address service unavailable.");
         return Flux.empty();
     }
     
     @GetMapping("/zip-code-validator")
     Flux<Void> getZipCodeValidatorFallback() {
+        LOGGER.error("Zip Validator service unavailable.");
         return Flux.empty();
     }
     
     @GetMapping("/user-service")
     Flux<Void> getUserServiceFallback() {
+        LOGGER.error("User service unavailable.");
         return Flux.empty();
     }
 
